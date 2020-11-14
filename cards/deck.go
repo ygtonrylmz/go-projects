d1a5c7929b3b01152f430d9078f84cfb62c2f785
Onur Yılmaz <ttygtonrylmz@gmail.com>
2020-11-14 18:51:31 +0300

    toString Added

--- cards/deck.go
+++ cards/deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 // Create a new type of deck
@@ -29,10 +30,17 @@ func newDeck() deck {
 // d = cards
 func (d deck) print() {
 	for i, card := range d {
-		fmt.Println(i,card)
+		fmt.Println(i, card)
 	}
 }
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	sliceStr := []string(d)
+	singleStr := strings.Join(sliceStr, ",")
+
+	return singleStr
 }
--- cards/main.go
+++ cards/main.go
@@ -1,11 +1,12 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 
 	cards := newDeck()
 
-	hand, remainingDeck := deal(cards,5)
-	
-	hand.print()
-	remainingDeck.print()
+	fmt.Println(cards.toString())
 }
